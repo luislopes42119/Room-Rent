17a868fd77b11e87ab8719ce0d86ff09a73e8958
Luis Miguel Lopes <l42119@alunos.uevora.pt>
2023-07-15 14:12:32 +0100

    inicial commit

---
+++ src/main/java/com/example/roomrent/RoomRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/roomrent/RoomRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roomrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
