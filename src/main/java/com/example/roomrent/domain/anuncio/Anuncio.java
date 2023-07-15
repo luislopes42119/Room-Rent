2cdb070e3d5676e8fa017474e2e020e28ee82e6f
Luis Miguel Lopes <l42119@alunos.uevora.pt>
2023-07-15 16:18:59 +0100

    create class Anuncio + upgrade the index

---
+++ src/main/java/com/example/roomrent/domain/anuncio/Anuncio.java
@@ -0,0 +1,47 @@
+package com.example.roomrent.domain.anuncio;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Anuncio {
+    private Long id;
+
+    private String local;
+    private int preco;
+    private char genero;
+    private String anunciante;
+    private int contacto;
+    private String tipologia;
+    private char tipo;
+    private Date data;
+    private char estado;
+
+    public static class AnuncioRowMapper implements RowMapper<Anuncio>{
+        @Override
+        public Anuncio mapRow(ResultSet rs, int rowNum) throws SQLException{
+            Anuncio anuncio = new Anuncio();
+            anuncio.setId(rs.getLong("id"));
+            anuncio.setLocal(rs.getString("local"));
+            anuncio.setPreco(rs.getInt("preco"));
+            anuncio.setGenero(rs.getString("genero").charAt(0));
+            anuncio.setAnunciante(rs.getString("anunciante"));
+            anuncio.setContacto(rs.getInt("contacto"));
+            anuncio.setTipologia(rs.getString("tipologia"));
+            anuncio.setTipo(rs.getString("tipo").charAt(0));
+            anuncio.setData(rs.getDate("data"));
+            anuncio.setEstado(rs.getString("estado").charAt(0));
+            return anuncio;
+        }
+    }
+}
